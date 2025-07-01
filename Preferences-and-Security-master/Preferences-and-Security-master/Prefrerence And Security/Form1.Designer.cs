079a15728bac39a6c9caf60720875d68690d38f8
JhakeLoide <J.jamahali.539655@umindanao.edu.ph>
2025-07-02 00:07:24 +0800

    Add files via upload

---
+++ Preferences-and-Security-master/Preferences-and-Security-master/Prefrerence And Security/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Prefrerence_And_Security
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnGeneral = new Button();
+            btnLogs = new Button();
+            btnBackupAndRestore = new Button();
+            btnPermission = new Button();
+            btnSecurity = new Button();
+            btnPreference = new Button();
+            btnNotification = new Button();
+            MainPanel = new Panel();
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // btnGeneral
+            // 
+            btnGeneral.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnGeneral.BackColor = Color.Transparent;
+            btnGeneral.DialogResult = DialogResult.OK;
+            btnGeneral.FlatAppearance.BorderSize = 0;
+            btnGeneral.FlatStyle = FlatStyle.Flat;
+            btnGeneral.ForeColor = Color.Gray;
+            btnGeneral.Location = new Point(66, 123);
+            btnGeneral.Margin = new Padding(3, 2, 3, 2);
+            btnGeneral.Name = "btnGeneral";
+            btnGeneral.Size = new Size(143, 22);
+            btnGeneral.TabIndex = 7;
+            btnGeneral.Text = "General";
+            btnGeneral.TextAlign = ContentAlignment.MiddleLeft;
+            btnGeneral.UseVisualStyleBackColor = false;
+            btnGeneral.Click += btnGeneral_Click;
+            // 
+            // btnLogs
+            // 
+            btnLogs.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnLogs.BackColor = Color.Transparent;
+            btnLogs.DialogResult = DialogResult.OK;
+            btnLogs.FlatAppearance.BorderSize = 0;
+            btnLogs.FlatStyle = FlatStyle.Flat;
+            btnLogs.ForeColor = Color.Gray;
+            btnLogs.Location = new Point(66, 284);
+            btnLogs.Margin = new Padding(3, 2, 3, 2);
+            btnLogs.Name = "btnLogs";
+            btnLogs.Size = new Size(143, 29);
+            btnLogs.TabIndex = 6;
+            btnLogs.Text = "Logs";
+            btnLogs.TextAlign = ContentAlignment.MiddleLeft;
+            btnLogs.UseVisualStyleBackColor = false;
+            btnLogs.Click += btnLogs_Click;
+            // 
+            // btnBackupAndRestore
+            // 
+            btnBackupAndRestore.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnBackupAndRestore.BackColor = Color.Transparent;
+            btnBackupAndRestore.DialogResult = DialogResult.OK;
+            btnBackupAndRestore.FlatAppearance.BorderSize = 0;
+            btnBackupAndRestore.FlatStyle = FlatStyle.Flat;
+            btnBackupAndRestore.ForeColor = Color.Gray;
+            btnBackupAndRestore.Location = new Point(66, 257);
+            btnBackupAndRestore.Margin = new Padding(3, 2, 3, 2);
+            btnBackupAndRestore.Name = "btnBackupAndRestore";
+            btnBackupAndRestore.Size = new Size(143, 22);
+            btnBackupAndRestore.TabIndex = 5;
+            btnBackupAndRestore.Text = "Back up and Restore";
+            btnBackupAndRestore.TextAlign = ContentAlignment.MiddleLeft;
+            btnBackupAndRestore.UseVisualStyleBackColor = false;
+            btnBackupAndRestore.Click += btnBackupAndRestore_Click;
+            // 
+            // btnPermission
+            // 
+            btnPermission.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnPermission.BackColor = Color.Transparent;
+            btnPermission.DialogResult = DialogResult.OK;
+            btnPermission.FlatAppearance.BorderSize = 0;
+            btnPermission.FlatStyle = FlatStyle.Flat;
+            btnPermission.ForeColor = Color.Gray;
+            btnPermission.Location = new Point(66, 203);
+            btnPermission.Margin = new Padding(3, 2, 3, 2);
+            btnPermission.Name = "btnPermission";
+            btnPermission.Size = new Size(143, 22);
+            btnPermission.TabIndex = 3;
+            btnPermission.Text = "Permissions";
+            btnPermission.TextAlign = ContentAlignment.MiddleLeft;
+            btnPermission.UseVisualStyleBackColor = false;
+            btnPermission.Click += btnPermission_Cilck;
+            // 
+            // btnSecurity
+            // 
+            btnSecurity.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnSecurity.BackColor = Color.Transparent;
+            btnSecurity.DialogResult = DialogResult.OK;
+            btnSecurity.FlatAppearance.BorderSize = 0;
+            btnSecurity.FlatStyle = FlatStyle.Flat;
+            btnSecurity.ForeColor = Color.Gray;
+            btnSecurity.Location = new Point(66, 176);
+            btnSecurity.Margin = new Padding(3, 2, 3, 2);
+            btnSecurity.Name = "btnSecurity";
+            btnSecurity.Size = new Size(143, 22);
+            btnSecurity.TabIndex = 2;
+            btnSecurity.Text = "Security";
+            btnSecurity.TextAlign = ContentAlignment.MiddleLeft;
+            btnSecurity.UseVisualStyleBackColor = false;
+            btnSecurity.Click += btnSecurity_Click;
+            // 
+            // btnPreference
+            // 
+            btnPreference.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnPreference.BackColor = Color.Transparent;
+            btnPreference.DialogResult = DialogResult.OK;
+            btnPreference.FlatAppearance.BorderSize = 0;
+            btnPreference.FlatStyle = FlatStyle.Flat;
+            btnPreference.ForeColor = Color.Gray;
+            btnPreference.Location = new Point(66, 150);
+            btnPreference.Margin = new Padding(3, 2, 3, 2);
+            btnPreference.Name = "btnPreference";
+            btnPreference.Size = new Size(143, 22);
+            btnPreference.TabIndex = 1;
+            btnPreference.Text = "Preference";
+            btnPreference.TextAlign = ContentAlignment.MiddleLeft;
+            btnPreference.UseVisualStyleBackColor = false;
+            btnPreference.Click += btnPreference_Click;
+            // 
+            // btnNotification
+            // 
+            btnNotification.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnNotification.BackColor = Color.Transparent;
+            btnNotification.DialogResult = DialogResult.OK;
+            btnNotification.FlatAppearance.BorderSize = 0;
+            btnNotification.FlatStyle = FlatStyle.Flat;
+            btnNotification.ForeColor = Color.Gray;
+            btnNotification.Location = new Point(66, 230);
+            btnNotification.Margin = new Padding(3, 2, 3, 2);
+            btnNotification.Name = "btnNotification";
+            btnNotification.Size = new Size(143, 22);
+            btnNotification.TabIndex = 4;
+            btnNotification.Text = "Notifications";
+            btnNotification.TextAlign = ContentAlignment.MiddleLeft;
+            btnNotification.UseVisualStyleBackColor = false;
+            btnNotification.Click += btnNotification_Click;
+            // 
+            // MainPanel
+            // 
+            MainPanel.BorderStyle = BorderStyle.Fixed3D;
+            MainPanel.Location = new Point(274, 56);
+            MainPanel.Margin = new Padding(3, 2, 3, 2);
+            MainPanel.Name = "MainPanel";
+            MainPanel.Size = new Size(1071, 700);
+            MainPanel.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(39, 60);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 696);
+            panel1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1406, 778);
+            Controls.Add(btnGeneral);
+            Controls.Add(MainPanel);
+            Controls.Add(btnLogs);
+            Controls.Add(btnBackupAndRestore);
+            Controls.Add(btnPermission);
+            Controls.Add(btnNotification);
+            Controls.Add(btnSecurity);
+            Controls.Add(btnPreference);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = " ";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button btnLogs;
+        private Button btnBackupAndRestore;
+        private Button btnPermission;
+        private Button btnSecurity;
+        private Button btnPreference;
+        private Button btnNotification;
+        private Button btnGeneral;
+        private Panel MainPanel;
+        private Panel panel1;
+    }
+}
---
+++ Preferences-and-Security-master/Preferences-and-Security-master/Prefrerence And Security/General.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace Prefrerence_And_Security
+{
+    partial class General
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            gridListControl1 = new Syncfusion.Windows.Forms.Grid.GridListControl();
+            cardLayout1 = new Syncfusion.Windows.Forms.Tools.CardLayout(components);
+            label1 = new Label();
+            treeView1 = new TreeView();
+            bannerTextProvider1 = new Syncfusion.Windows.Forms.BannerTextProvider(components);
+            treeView2 = new TreeView();
+            treeView3 = new TreeView();
+            treeView4 = new TreeView();
+            treeView5 = new TreeView();
+            treeView6 = new TreeView();
+            treeView7 = new TreeView();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            ((System.ComponentModel.ISupportInitialize)gridListControl1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cardLayout1).BeginInit();
+            SuspendLayout();
+            // 
+            // gridListControl1
+            // 
+            gridListControl1.BackColor = SystemColors.Control;
+            gridListControl1.BorderStyle = BorderStyle.Fixed3D;
+            gridListControl1.ItemHeight = 17;
+            gridListControl1.Location = new Point(192, 213);
+            gridListControl1.Name = "gridListControl1";
+            gridListControl1.Properties.BackgroundColor = SystemColors.Window;
+            gridListControl1.SelectedIndex = -1;
+            gridListControl1.Size = new Size(90, 28);
+            gridListControl1.TabIndex = 0;
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ArrowButtonBackColor = Color.FromArgb(255, 255, 255);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ArrowButtonBorderColor = Color.FromArgb(225, 225, 225);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ArrowButtonDisabledBackColor = Color.FromArgb(225, 225, 225);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ArrowButtonDisabledBorderColor = Color.FromArgb(210, 210, 210);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ArrowButtonHoverBackColor = Color.FromArgb(114, 114, 114);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ArrowButtonHoverBorderColor = Color.FromArgb(94, 94, 94);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ArrowButtonPressedBackColor = Color.FromArgb(225, 225, 225);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ArrowButtonPressedBorderColor = Color.FromArgb(150, 150, 150);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ScrollBarBackColor = Color.FromArgb(225, 225, 225);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ThumbBorderColor = Color.FromArgb(171, 171, 171);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ThumbColor = Color.FromArgb(255, 255, 255);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ThumbDisabledBorderColor = Color.FromArgb(210, 210, 210);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ThumbDisabledColor = Color.FromArgb(225, 225, 225);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ThumbHoverBorderColor = Color.FromArgb(171, 171, 171);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ThumbHoverColor = Color.FromArgb(197, 197, 197);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ThumbPressedBorderColor = Color.FromArgb(150, 150, 150);
+            gridListControl1.ThemeStyle.HorizontalScrollBarStyle.ThumbPressedColor = Color.FromArgb(197, 197, 197);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ArrowButtonBackColor = Color.FromArgb(255, 255, 255);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ArrowButtonBorderColor = Color.FromArgb(225, 225, 225);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ArrowButtonDisabledBackColor = Color.FromArgb(225, 225, 225);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ArrowButtonDisabledBorderColor = Color.FromArgb(210, 210, 210);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ArrowButtonHoverBackColor = Color.FromArgb(114, 114, 114);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ArrowButtonHoverBorderColor = Color.FromArgb(94, 94, 94);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ArrowButtonPressedBackColor = Color.FromArgb(225, 225, 225);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ArrowButtonPressedBorderColor = Color.FromArgb(150, 150, 150);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ScrollBarBackColor = Color.FromArgb(225, 225, 225);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ThumbBorderColor = Color.FromArgb(171, 171, 171);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ThumbColor = Color.FromArgb(255, 255, 255);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ThumbDisabledBorderColor = Color.FromArgb(210, 210, 210);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ThumbDisabledColor = Color.FromArgb(225, 225, 225);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ThumbHoverBorderColor = Color.FromArgb(171, 171, 171);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ThumbHoverColor = Color.FromArgb(197, 197, 197);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ThumbPressedBorderColor = Color.FromArgb(150, 150, 150);
+            gridListControl1.ThemeStyle.VerticalScrollBarStyle.ThumbPressedColor = Color.FromArgb(197, 197, 197);
+            gridListControl1.TopIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(59, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(297, 45);
+            label1.TabIndex = 0;
+            label1.Text = "System Information";
+            // 
+            // treeView1
+            // 
+            treeView1.Location = new Point(82, 199);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(1075, 56);
+            treeView1.TabIndex = 1;
+            // 
+            // treeView2
+            // 
+            treeView2.Location = new Point(82, 355);
+            treeView2.Name = "treeView2";
+            treeView2.Size = new Size(523, 56);
+            treeView2.TabIndex = 2;
+            // 
+            // treeView3
+            // 
+            treeView3.Location = new Point(82, 514);
+            treeView3.Name = "treeView3";
+            treeView3.Size = new Size(1075, 56);
+            treeView3.TabIndex = 3;
+            // 
+            // treeView4
+            // 
+            treeView4.Location = new Point(82, 278);
+            treeView4.Name = "treeView4";
+            treeView4.Size = new Size(1075, 56);
+            treeView4.TabIndex = 4;
+            // 
+            // treeView5
+            // 
+            treeView5.Location = new Point(634, 355);
+            treeView5.Name = "treeView5";
+            treeView5.Size = new Size(523, 56);
+            treeView5.TabIndex = 5;
+            // 
+            // treeView6
+            // 
+            treeView6.Location = new Point(82, 434);
+            treeView6.Name = "treeView6";
+            treeView6.Size = new Size(1075, 56);
+            treeView6.TabIndex = 6;
+            // 
+            // treeView7
+            // 
+            treeView7.Location = new Point(82, 600);
+            treeView7.Name = "treeView7";
+            treeView7.Size = new Size(1075, 56);
+            treeView7.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(87, 188);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 20);
+            label2.TabIndex = 8;
+            label2.Text = "System Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(109, 218);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 20);
+            label3.TabIndex = 9;
+            label3.Text = "U.M.E.C.A";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(109, 297);
+            label4.Name = "label4";
+            label4.Size = new Size(28, 20);
+            label4.TabIndex = 10;
+            label4.Text = "0.1";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(109, 533);
+            label5.Name = "label5";
+            label5.Size = new Size(253, 20);
+            label5.TabIndex = 11;
+            label5.Text = "support.UMECA.@umindanao.edu.ph";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(109, 451);
+            label6.Name = "label6";
+            label6.Size = new Size(197, 20);
+            label6.TabIndex = 12;
+            label6.Text = "Second Semester 2024-2025";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(109, 371);
+            label7.Name = "label7";
+            label7.Size = new Size(66, 20);
+            label7.TabIndex = 13;
+            label7.Text = "#1a3a6e";
+            label7.Click += label7_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(109, 619);
+            label8.Name = "label8";
+            label8.Size = new Size(97, 20);
+            label8.TabIndex = 14;
+            label8.Text = "09956435975";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(662, 371);
+            label9.Name = "label9";
+            label9.Size = new Size(67, 20);
+            label9.TabIndex = 15;
+            label9.Text = "#3066be";
+            label9.Click += label9_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(87, 268);
+            label10.Name = "label10";
+            label10.Size = new Size(108, 20);
+            label10.TabIndex = 16;
+            label10.Text = "System Version";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(87, 346);
+            label11.Name = "label11";
+            label11.Size = new Size(99, 20);
+            label11.TabIndex = 17;
+            label11.Text = "Primary Color";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(643, 346);
+            label12.Name = "label12";
+            label12.Size = new Size(118, 20);
+            label12.TabIndex = 18;
+            label12.Text = "Secondary Color";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(87, 588);
+            label13.Name = "label13";
+            label13.Size = new Size(118, 20);
+            label13.TabIndex = 19;
+            label13.Text = "Contact Number";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(87, 504);
+            label14.Name = "label14";
+            label14.Size = new Size(156, 20);
+            label14.TabIndex = 20;
+            label14.Text = "System Contact Email ";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(88, 425);
+            label15.Name = "label15";
+            label15.Size = new Size(122, 20);
+            label15.TabIndex = 21;
+            label15.Text = "Current Semester";
+            // 
+            // General
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1223, 886);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(treeView7);
+            Controls.Add(treeView6);
+            Controls.Add(treeView5);
+            Controls.Add(treeView4);
+            Controls.Add(treeView3);
+            Controls.Add(treeView2);
+            Controls.Add(treeView1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "General";
+            Text = "  ";
+            Load += General_Load;
+            ((System.ComponentModel.ISupportInitialize)gridListControl1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cardLayout1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Grid.GridListControl gridListControl1;
+        private Syncfusion.Windows.Forms.Tools.CardLayout cardLayout1;
+        private Label label1;
+        private TreeView treeView1;
+        private Syncfusion.Windows.Forms.BannerTextProvider bannerTextProvider1;
+        private TreeView treeView2;
+        private TreeView treeView3;
+        private TreeView treeView4;
+        private TreeView treeView5;
+        private TreeView treeView6;
+        private TreeView treeView7;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+    }
+}
