7f3ee35f45e3d56758824426c138918e082d27a2
John Andrei Lee <67588993+jandreeeh@users.noreply.github.com>
2025-07-02 13:54:11 +0800

    assembly info lol

--- Consultation.App/obj/Debug/net8.0-windows/Consultation.App.AssemblyInfo.cs
+++ Consultation.App/obj/Debug/net8.0-windows/Consultation.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ce05c3284e6ee4e946e5a13dd7ebd1b60433f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9c8cf08580aaa511dedbc78801241317dce8f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Consultation.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
